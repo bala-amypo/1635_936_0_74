7624df5983eb9b467da7552463c9825851beb54f
bala-amypo <bala@amypo.in>
2025-12-18 07:57:21 +0000

    User commit

--- src/main/java/com/example/demo/entity/ValidationEntity.java
+++ src/main/java/com/example/demo/entity/ValidationEntity.java
@@ -3,5 +3,11 @@ package com.example.demo.entity;
 import jakarta.persistence.Entity;
 @Entity
 public class ValidationEntity{
-
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String email;
+    private String password;
+    private int age;
 }
