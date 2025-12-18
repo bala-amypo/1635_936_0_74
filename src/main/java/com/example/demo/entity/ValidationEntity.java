7c35d82675a917287093b57a1a0d4a8ac9be15c1
bala-amypo <bala@amypo.in>
2025-12-18 09:49:38 +0000

    User commit

--- src/main/java/com/example/demo/entity/ValidationEntity.java
+++ src/main/java/com/example/demo/entity/ValidationEntity.java
@@ -1,74 +1,74 @@
 package com.example.demo.entity;
-
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Email;
 @Entity
-public class ValidationEntity{
-    @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
-    private Long id;
-    @NotNull
-    @Size(min=2, max=10,message="must be 2 to 10 character")
-    private String username;
-    @Email (message="Email is not valid")
-    private String email;
-    @Size(min=2, max=6,message="Password must be 2 to 6 character")
-    @NotNull(message="Password is mandatory")
-    private String password;
-    @Max(30)
-    @Positive(message="Age must be Positive")
-    private int age;
+public class validationEntity{
+ @Id
+ @GeneratedValue(strategy=GenerationType.IDENTITY)
+private Long id;
+ @NotNull
+ @Size(min = 1,max = 6,message = "must be 2 to 6 characters")
+private String username;
+ @Email(message ="Email is not valid")
+private String email;
+ @Size(min = 2,max = 6)
+ @NotNull(message = "Password is mandatory")
+private String password;
+ @Max(30)
+ @Positive(message = "Age must be a positive number")
+private int age;
+ public void setId(Long id){
+    this.id=id;
+ }
+ public void setUsername(String username){
+    this.username=username;
+ }
+ public void setEmail(String email){
+    this.email=email;
+ }
+ public void setPassword(String password){
+    this.password=password;
+ }
+ public void setAge(int age){
+    this.age=age;
+ }
+ public Long getId(){
+    return id;
+ }
+ public String getUsername(){
+    return username;
+ }
+ public String getEmail(){
+    return email;
+ }
+ public String getPassword(){
+    return password;
+ }
+ public int getAge(){
+    return age;
+ }
+ public validationEntity(){
 
-     public void setId(Long id){
-        this.id=id;
-    }
-    public Long getId(){
-        return id;
-    }
-    public void setUserName(String username){
-        this.username=username;
-    }
-    public String getUserName(){
-        return username;
-    }
-    public String getEmail(){
-        return email;
-    }
-    public void setEmail(String email){
-        this.email=email;
-    }
-     public String getPassword(){
-        return password;
-    }
-    public void setPassword(String password){
-        this.password=password;
-    }
-     public void setAge(int age){
-        this.age=age;
-    }
-    public int getAge(){
-        return age;
-    }
-    public ValidationEntity(Long id,
-     @NotNull @Size(min=2, max=10,message="must be 2 to 10 character") String username,
-     @Email (message="Email is not valid") String email,
-     @Size(min=2, max=6,message="Password must be 2 to 6 character")
-    @NotNull(message="Password is mandatory") String password,
-    @Max(30) @Positive(message="Age must be Positive") int age){
-        this.id=id;
-        this.username=username;
-        this.email=email;
-        this.password=password;
-        this.age=age;
-    }
-    public ValidationEntity(){
-
-    }
+ }
+ public validationEntity(Long id,
+       @NotNull
+       @Size(min = 1,max = 6,message = "must be 2 to 6 characters")String username,
+       @Email(message= "Email is not valid")String email,
+       @Size(min = 2,max = 6,message ="Password must be 2 to 6 characters")
+       @NotNull(message = "Password is mandatory")String password,
+       @Max(30) @Positive(message = "Age must be a positive number")int age){
+    this.id=id;
+    this.username=username;
+    this.email=email;
+    this.password=password;
+    this.age=age;
+ }
 }
+
