11b30bcc1d38a42c68c5b55c884bd75e116a2396
bala-amypo <bala@amypo.in>
2025-12-19 05:05:48 +0000

    User commit

--- src/main/java/com/example/demo/controller/ValidationController.java
+++ src/main/java/com/example/demo/controller/ValidationController.java
@@ -19,7 +19,7 @@ public class ValidationController{
         return ser.PostData(stu);
     }
     @GetMapping("/gettid/{id}")
-    public ValidationEntity getData(@PathVariable int id){
+    public ValidationEntity getData(@PathVariable Long id){
         return ser.getData(id);
 }
 }
